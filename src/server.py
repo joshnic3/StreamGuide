4e92c270a744e959c89c3e90ef0bc8234bc56754
Josh Nicholls <josh.nicholls@yahoo.co.uk>
2020-11-27 18:24:32 +0000

    added temp response header from origin print

--- src/server.py
+++ src/server.py
@@ -52,6 +52,7 @@ def listings():
     search_string = request.args.get('search', default='', type=str).lower().replace('+', ' ')
     filter_list = request.cookies.get('service_filter').split(',')
     print(request.cookies)
+    print(request.headers)
     request_id = track_request(request.cookies.get('uid'), constants.SERVER.GET, parameters={'search': search_string})
 
     # Get requested data from API.
